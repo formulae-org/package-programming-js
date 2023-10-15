bc9f1e00635c8cb1c6bc8315a8c943f11937862d
Laurence R. Ugalde <laurence.ugalde@formulae.org>
2023-10-14 19:33:53 -0600

    added description to reducers

--- reduction.js
+++ reduction.js
@@ -386,16 +386,16 @@ Programming.conditionalSwitch = async (_switch, session) => {
 };
 
 Programming.setReducers = () => {
-	ReductionManager.addReducer("Programming.Block",             Programming.blockReducer);
-	ReductionManager.addReducer("Programming.If",                Programming.ifReducer,         { special: true });
-	ReductionManager.addReducer("Programming.InvertedIf",        Programming.invertedIfReducer, { special: true });
-	ReductionManager.addReducer("Programming.IfElse",            Programming.ifElseReducer,     { special: true });
-	ReductionManager.addReducer("Programming.Conditional",       Programming.ifElseReducer,     { special: true });
-	ReductionManager.addReducer("Programming.ForTimes",          Programming.forTimesReducer,   { special: true });
-	ReductionManager.addReducer("Programming.ForFromTo",         Programming.forFromToReducer,  { special: true });
-	ReductionManager.addReducer("Programming.ForIn",             Programming.forInReducer,      { special: true });
-	ReductionManager.addReducer("Programming.While",             Programming.whileReducer,      { special: true });
-	ReductionManager.addReducer("Programming.Until",             Programming.untilReducer,      { special: true });
-	ReductionManager.addReducer("Programming.ComparativeSwitch", Programming.comparativeSwitch, { special: true });
-	ReductionManager.addReducer("Programming.ConditionalSwitch", Programming.conditionalSwitch, { special: true });
+	ReductionManager.addReducer("Programming.Block",             Programming.blockReducer,      "Programming.blockReducer");
+	ReductionManager.addReducer("Programming.If",                Programming.ifReducer,         "Programming.ifReducer", { special: true });
+	ReductionManager.addReducer("Programming.InvertedIf",        Programming.invertedIfReducer, "Programming.invertedIfReducer", { special: true });
+	ReductionManager.addReducer("Programming.IfElse",            Programming.ifElseReducer,     "Programming.ifElseReducer", { special: true });
+	ReductionManager.addReducer("Programming.Conditional",       Programming.ifElseReducer,     "Programming.ifElseReducer", { special: true });
+	ReductionManager.addReducer("Programming.ForTimes",          Programming.forTimesReducer,   "Programming.forTimesReducer", { special: true });
+	ReductionManager.addReducer("Programming.ForFromTo",         Programming.forFromToReducer,  "Programming.forFromToReducer", { special: true });
+	ReductionManager.addReducer("Programming.ForIn",             Programming.forInReducer,      "Programming.forInReducer", { special: true });
+	ReductionManager.addReducer("Programming.While",             Programming.whileReducer,      "Programming.whileReducer", { special: true });
+	ReductionManager.addReducer("Programming.Until",             Programming.untilReducer,      "Programming.untilReducer", { special: true });
+	ReductionManager.addReducer("Programming.ComparativeSwitch", Programming.comparativeSwitch, "Programming.comparativeSwitch", { special: true });
+	ReductionManager.addReducer("Programming.ConditionalSwitch", Programming.conditionalSwitch, "Programming.conditionalSwitch", { special: true });
 };
